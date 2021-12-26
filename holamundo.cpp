b64619f53932bbb46c6d5f8072f64aa16406ceff
Javier Peraza <contacto.peraza@gmail.com>
2021-12-26 11:09:15 -0600

    se agregó comentarios, listo merge al master

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
+
+//las librerias se agregan de esta forma
+#include <iostream> //iostream es una libreria de C++ para la entrada y la salida de datos
 
 using namespace std;
 
+/*En C/C++ el programa ocurre dentro del Metodo Principal Main*/
 int main()
+
 {
-    cout<< printf ("Hola mundo esta es una prueba de compilación \n salto de linea? ");
+    cout<< printf ("Hola mundo esta es una prueba de compilación \n salto de linea? ");  
+    //cout y cin son de la libreria de iostream
 
 
     return 0;
