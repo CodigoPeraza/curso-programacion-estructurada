4150a12dd853b0b8a59f3ae8909fa310579fe211
Javier Peraza <contacto.peraza@gmail.com>
2021-12-26 18:02:38 -0600

    decidí hacer un menú para documentar lo aprendido, case 1 y 2 resuelto

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,6 +1,9 @@
 //este es la rama testing
 //las librerias se agregan de esta forma
 #include <iostream> //iostream es una libreria de C++ para la entrada y la salida de datos
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
@@ -8,9 +11,35 @@ using namespace std;
 int main()
 
 {
-    cout<< printf ("Hola mundo esta es una prueba de compilación \n salto de linea? ");  
+    int menu;
+    cout<< "Repasemos lo aprendido \n";  
     //cout y cin son de la libreria de iostream
 
+    cout<< ("Elije una opción. \n1. Tipos de datos. \n2. Entrada y salida de datos. \n3. Estructuras de control. \n4. condicionales.\n");
+    cin >>menu;
+
+    switch (menu)
+    {
+    case 1: {
+
+        cout<< " \n int entero = 20; \n float decimal = 20.5; \n string cadena = *entre comillas -->texto \n char caracter = 'q'; \n bool booleano = false; \n\n";
+        int entero = 20;
+        float decimal = 20.5;
+        string cadena = "texto";
+        char caracter = 'q';
+        bool booleano = false;
+
+        }break; 
+        
+    case 2: {
+        cout<< " cout<< salida de datos \n cin>>entrada de datos por consola \n";
+        }break;
+    
+    default:{
+        cout << "nunca pares de aprender";
+        }break;
+    
+    }
 
     return 0;
 }
