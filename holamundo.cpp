59f2fa8229e8f5f1ab614ef9faef3b528b6f6bb3
Javier Peraza <contacto.peraza@gmail.com>
2021-12-30 20:19:14 -0600

    se añadió ejemplo DO WHILE pero esta BUG, se tiene que revisar y arreglar

--- holamundo.cpp
+++ holamundo.cpp
@@ -54,6 +54,7 @@ int main()
             /*=============================MENÚ=============================*/
             cout<< "1. bucle while \n";
             cout<< "2. bucle for \n";
+            cout<< "3. bucle do while";
             cout<< "\n elige una opcion: ";
             cin>> opcion;
             system("clear"); 
@@ -63,73 +64,123 @@ int main()
             
             switch (opcion)
             {
-            case 1:{
+                case 1:{
 
-                /*============================= WHILE =============================*/
-                cout<<"\nwhile (condición)\n";
-                cout<<"{ \n";
-                cout<<"     código que se ejecuta \n";
-                cout<<"     se recomienda usar un iterador ejemplo --> int i++;\n";
-                cout<<"     para evitar un ciclo infinito\n";
-                cout<< "} \n";                
-                                
-                cout<<"/*========================== EJEMPLO WHILE ========================*/\n\n";
-                cout<<"imprime n numeros\n\n";
-                cout<<"¿cuantos números desea imprimir? \n";
-                cout<<":";
+                    /*============================= WHILE =============================*/
+                    cout<<"\nwhile (condición)\n";
+                    cout<<"{ \n";
+                    cout<<"     código que se ejecuta \n";
+                    cout<<"     se recomienda usar un iterador ejemplo --> int i++;\n";
+                    cout<<"     para evitar un ciclo infinito\n";
+                    cout<< "} \n";                
+                                    
+                    cout<<"/*========================== EJEMPLO WHILE ========================*/\n\n";
+                    cout<<"imprime n numeros\n\n";
+                    cout<<"¿cuantos números desea imprimir? \n";
+                    cout<<":";
 
-                short n;
-                cin>>n; 
-                system("clear"); 
-                //NOTA: si se va a compilar para windows cambiar a system("cls");
+                    short n;
+                    cin>>n; 
+                    system("clear"); 
+                    //NOTA: si se va a compilar para windows cambiar a system("cls");
+                    
+
+                    int i = 0;            
+                    while (i <= n)
+                    {
+                        int numero = i;
+                        cout<< numero; //imprime el valor de i
+                        cout<<"\n";
+                        i++; //incrementa el valor de i
+
+                    }
+                    
+
+                    /*=================================================================*/
+                    }break;
                 
+                
+                case 2: {
 
-                int i = 0;            
-                while (i <= n)
-                {
-                    int numero = i;
-                    cout<< numero; //imprime el valor de i
-                    cout<<"\n";
-                    i++; //incrementa el valor de i
+                    /*============================= FOR =============================*/
+                    cout<<"for (i = 0; i >= 10; i++)\n";
+                    cout<<"    //contador//condicional//incremento-decremento\n";
+                    cout<<"{\n\n";
+                    cout<<"codigo\n\n";    
+                    cout<<"}\n\n";
+                    
+                    cout<<"/*=========================EJEMPLO FOR==============================*/\n\n";
+                    cout<<"sucesión de fibonacci\n\n";
+                    int contador;
 
+                    cout<< "Cuantos elementos de la sucesión quiere: ";
+                    cin>> contador;
+                    cout<<"\n\n";
+                    
+                    long a = 0;
+                    long b = 1;
+                    long c;
+                    for (int i = 0; i < contador; i++)
+                    {
+                        cout<< a; 
+                        c = a+b;
+                        a = b;
+                        b = c;
+                        cout<<"\n";
+                    }
+                    /*==================================================================*/
+                    }break;
+                case 3: {
+                    /*============================= DO WHILE =============================*/
+
+                    cout<<"do\n";
+                    cout<<"{\n\n";
+                    cout<<"    /* codigo */\n\n";
+                    cout<<"} while (/* condición */);\n";                 
+                    
+                    cout<<"/*==================== EJEMPLO DE DO WHILE =======================*/\n";
+                    cout<<"menú con opción para salir\n";
+
+                    do
+                    {
+                        system("clear"); 
+                        //NOTA: si se va a compilar para windows cambiar a system("cls");
+                        cout<<"Elije una opción: \n\n";
+                        cout<<"1. decir hola mundo\n";
+                        cout<<"2. decir hacta luego\n";
+                        cout<<"3. salir\n";
+                        int opcion1;
+                        cin>>opcion1;
+                        switch (opcion1)
+                        {
+                        case 1: {
+
+                            cout<<"hola mundo!!! :) \n";
+                        }break;
+                        
+                        case 2: {
+                            cout<<"Hasta luego!!! :D  \n";
+                        }
+
+                        case 3: {
+                            int opcion1 = 3;
+                        }
+
+                        default: {
+
+                            cout<<"elija una opción valida: \n";
+                            break;
+                        }
+                        }
+
+                    }while (opcion1 == 3);
+                        
                 }
+            } //fin del switch del case 3
+
                 
 
-                /*=================================================================*/
-                }break;
             
-            
-            case 2: {
-
-                /*============================= FOR =============================*/
-                cout<<"for (i = 0; i >= 10; i++)\n";
-                cout<<"    //contador//condicional//incremento-decremento\n";
-                cout<<"{\n\n";
-                cout<<"codigo\n\n";    
-                cout<<"}\n\n";
-                
-                /*=========================EJEMPLO FOR==============================*/
-                /*========================= FIBONACCI ==============================*/
-                int contador;
-
-                cout<< "Cuantos elementos de la sucesión quiere: ";
-                cin>> contador;
-                
-                long a = 0;
-                long b = 1;
-                long c;
-                for (int i = 0; i < contador; i++)
-                {
-                    cout<< a; 
-                    c = a+b;
-                    a = b;
-                    b = c;
-                    cout<<"\n";
-                }
-                /*==================================================================*/
-                }break;
-
-            } //fin de swich case3
 
         }break; //break del case 3
         
